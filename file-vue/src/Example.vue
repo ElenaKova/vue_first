38e3a0b53dd4035ff4822353466c791bcd8f8e06
Eleko <ikovalenko1999@gmail.com>
2022-07-05 12:31:24 +0200

    Add vue-cli. Two file vue

---
+++ file-vue/src/Example.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Text</h1>
+        <app-input>
+            
+        </app-input> </div>
+</template>
+
+<script>
+
+import Input from './Field.vue';
+
+export default {
+    components: {
+        'app-input': Input
+    }
+}
+
+</script>
