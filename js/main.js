2960a8909953f9ab74bff7a684592ab7bc7cb60d
Eleko <ikovalenko1999@gmail.com>
2022-07-05 11:24:06 +0200

    Adding global components. When the buttons are pressed, the value of the global variable count increase by 1. The value of the variables as an inscription on the buttons.

--- js/main.js
+++ js/main.js
@@ -43,134 +43,143 @@
 //     template: '<div><div class="list-country" v-for="name in name_of_country"> <p> Country: {{name.country}} <br> Capital: {{ name.capital}} </p> </div></div>'
 // });
 
+Vue.component('simple-counter', {
+    data: function () {
+        return {
+            count: 0
+        };
+    },
+    template: '<button id="components-count" v-on:click="count++"> {{count}} </button>',
+});
+
 let app = new Vue({
     el: '#app',
-    data: {
-        show: false,
-        title: 'Countries and their Capitals',
-        number: 25000,
-        date: '10-02-1999',
+    // data: {
+    //     show: false,
+    //     title: 'Countries and their Capitals',
+    //     number: 25000,
+    //     date: '10-02-1999',
 
-        //     name_of_country: [{
-        //             country: 'Albania',
-        //             capital: 'Tirana'
-        //         },
-        //         {
-        //             country: 'Andorra',
-        //             capital: 'Andorra la Vella'
-        //         },
-        //         {
-        //             country: 'Austria',
-        //             capital: 'Vienna'
-        //         },
-        //         {
-        //             country: 'Belarus',
-        //             capital: 'Minsk'
-        //         },
-        //         {
-        //             country: 'Belgium',
-        //             capital: 'Brussels'
-        //         },
-        //         {
-        //             country: 'Bosnia and Herzegovina',
-        //             capital: 'Sarajevo'
-        //         }
-        // Bulgaria: 'Sofia',
-        // croatia: 'Zagreb',
-        // czechia :'Prague',
-        // Denmark :'copenhagen',
-        // Estonia :'Tallinn',
-        // Finland :'Helsinki',
-        // France :'Paris',
-        // Germany: 'Berlin',
-        // Greece :'Athens',
-        // Hungary: 'Budapest',
-        // Iceland: 'Reykjavik',
-        // Ireland: 'Dublin',
-        // Italy :'Rome',
-        // Latvia :'Riga',
-        // Liechtenstein :'Vaduz',
-        // Lithuania: 'Vilnius',
-        // Luxembourg: 'Luxembourg',
-        // Malta: 'Valletta',
-        // Moldova: 'chisinau',
-        // Monaco :'Monaco',
-        // Montenegro: 'Podgorica',
-        // Netherlands: 'Amsterdam',
-        // North Macedonia: 'Skopje',
-        // Norway :'Oslo',
-        // Poland :'Warsaw',
-        // Portugal: 'Lisbon',
-        // Romania: 'Bucharest',
-        // San Marino: 'San Marino',
-        // Serbia: 'Belgrade',
-        // Slovakia: 'Bratislava',
-        // Slovenia :'Ljubljana',
-        // Spain :'Madrid',
-        // Sweden: 'Stockholm',
-        // Switzerland: 'Bern',
-        // Ukraine:'Kyiv',
-        // United: 'Kingdom London'
-        //     ]
-    },
-    components: {
-        'app-name_of_country': {
-            data: function () {
-                return {
-                    name_of_country: [{
-                            country: 'Albania',
-                            capital: 'Tirana'
-                        },
-                        {
-                            country: 'Andorra',
-                            capital: 'Andorra la Vella'
-                        },
-                        {
-                            country: 'Austria',
-                            capital: 'Vienna'
-                        },
-                        {
-                            country: 'Belarus',
-                            capital: 'Minsk'
-                        },
-                        {
-                            country: 'Belgium',
-                            capital: 'Brussels'
-                        },
-                        {
-                            country: 'Bosnia and Herzegovina',
-                            capital: 'Sarajevo'
-                        }
-                    ]
-                };
-            },
-            template: '<div><div class="list-country" v-for="name in name_of_country"> <p> Country: {{name.country}} <br> Capital: {{ name.capital}} </p> </div></div>'
-        }
-    },
+    //     name_of_country: [{
+    //             country: 'Albania',
+    //             capital: 'Tirana'
+    //         },
+    //         {
+    //             country: 'Andorra',
+    //             capital: 'Andorra la Vella'
+    //         },
+    //         {
+    //             country: 'Austria',
+    //             capital: 'Vienna'
+    //         },
+    //         {
+    //             country: 'Belarus',
+    //             capital: 'Minsk'
+    //         },
+    //         {
+    //             country: 'Belgium',
+    //             capital: 'Brussels'
+    //         },
+    //         {
+    //             country: 'Bosnia and Herzegovina',
+    //             capital: 'Sarajevo'
+    //         }
+    // Bulgaria: 'Sofia',
+    // croatia: 'Zagreb',
+    // czechia :'Prague',
+    // Denmark :'copenhagen',
+    // Estonia :'Tallinn',
+    // Finland :'Helsinki',
+    // France :'Paris',
+    // Germany: 'Berlin',
+    // Greece :'Athens',
+    // Hungary: 'Budapest',
+    // Iceland: 'Reykjavik',
+    // Ireland: 'Dublin',
+    // Italy :'Rome',
+    // Latvia :'Riga',
+    // Liechtenstein :'Vaduz',
+    // Lithuania: 'Vilnius',
+    // Luxembourg: 'Luxembourg',
+    // Malta: 'Valletta',
+    // Moldova: 'chisinau',
+    // Monaco :'Monaco',
+    // Montenegro: 'Podgorica',
+    // Netherlands: 'Amsterdam',
+    // North Macedonia: 'Skopje',
+    // Norway :'Oslo',
+    // Poland :'Warsaw',
+    // Portugal: 'Lisbon',
+    // Romania: 'Bucharest',
+    // San Marino: 'San Marino',
+    // Serbia: 'Belgrade',
+    // Slovakia: 'Bratislava',
+    // Slovenia :'Ljubljana',
+    // Spain :'Madrid',
+    // Sweden: 'Stockholm',
+    // Switzerland: 'Bern',
+    // Ukraine:'Kyiv',
+    // United: 'Kingdom London'
+    //     ]
+    // },
+    // components: {
+    //     'app-name_of_country': {
+    //         data: function () {
+    //             return {
+    //                 name_of_country: [{
+    //                         country: 'Albania',
+    //                         capital: 'Tirana'
+    //                     },
+    //                     {
+    //                         country: 'Andorra',
+    //                         capital: 'Andorra la Vella'
+    //                     },
+    //                     {
+    //                         country: 'Austria',
+    //                         capital: 'Vienna'
+    //                     },
+    //                     {
+    //                         country: 'Belarus',
+    //                         capital: 'Minsk'
+    //                     },
+    //                     {
+    //                         country: 'Belgium',
+    //                         capital: 'Brussels'
+    //                     },
+    //                     {
+    //                         country: 'Bosnia and Herzegovina',
+    //                         capital: 'Sarajevo'
+    //                     }
+    //                 ]
+    //             };
+    //         },
+    //         template: '<div><div class="list-country" v-for="name in name_of_country"> <p> Country: {{name.country}} <br> Capital: {{ name.capital}} </p> </div></div>'
+    //     }
+    // },
 
-    methods: {
+    // methods: {
 
-    },
-    computed: {
-        // ShowMess() {
-        //     return this.title.toUpperCase();
-        // }
-    },
-    filters: {
-        lowerCase(value) {
-            return value.toLowerCase();
-        },
+    // },
+    // computed: {
+    //     // ShowMess() {
+    //     //     return this.title.toUpperCase();
+    //     // }
+    // },
+    // filters: {
+    //     lowerCase(value) {
+    //         return value.toLowerCase();
+    //     },
 
-        dateFilter(value) {
-            let monthNames = [
-                "January", "February", "March",
-                "April", "May", "June", "July",
-                "August", "September", "October",
-                "November", "December"
-            ];
-            let dateArr = value.split("-");
-            dateArr[1] = monthNames[parseInt(dateArr[1]) - 1];
-            return dateArr[0] + " " + dateArr[1] + " " + dateArr[2] + " year";
-        }
-    }
+    //     dateFilter(value) {
+    //         let monthNames = [
+    //             "January", "February", "March",
+    //             "April", "May", "June", "July",
+    //             "August", "September", "October",
+    //             "November", "December"
+    //         ];
+    //         let dateArr = value.split("-");
+    //         dateArr[1] = monthNames[parseInt(dateArr[1]) - 1];
+    //         return dateArr[0] + " " + dateArr[1] + " " + dateArr[2] + " year";
+    //     }
+    // }
 });
